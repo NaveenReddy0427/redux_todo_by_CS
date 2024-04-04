151567ae38aad2aeae8b142c7d41d1a12bd5330b
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-04 16:18:28 +0530

    modify some code

--- src/store.js
+++ src/store.js
@@ -1,34 +1,43 @@
-import { createStore } from "redux"
+import { createStore } from "redux";
 
-const initialState = {
-    todos: [],
-    nextId: 1,
-}
+const initialstate = {
+  todos: [{ id: 1, title: "goto movie" }],
+  nextId: 2,
+};
 
-function todoReducer(state=initialState, action){
-    switch(action.type){
-        case "ADD_TODO":
-            const newTodo = {id:state.nextId, title: action.payload.title}
-            return{
-                ...state, todos:[...state.todos, newTodo], nextId: state.nextId + 1
-            }
-        case "EDIT_TODO":
-            return{
-                ...state, todos: state.todos.map(
-                    (todo)=>todo.id === action.payload.id?{...todo, title:action.payload.title}:todo
-                )
-            }
-        case "DELETE_TODO":
-            return{
-                ...state, todos: state.todos.filter(
-                    (todo)=>todo.id !== action.payload
-                )
-            }
-        default:
-            return state
-    }
-}
+const todoReducer = (state = initialstate, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      const newTodo = { id: state.nextId, title: action.payload.title };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        nextId: state.nextId + 1,
+      };
 
-const store = createStore(todoReducer)
+    //   {id:,title}
 
-export default store
+    case "EDIT_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, title: action.payload.title }
+            : todo
+        ),
+      };
+
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(todoReducer);
+
+export default store;
