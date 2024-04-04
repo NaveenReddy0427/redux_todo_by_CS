a3996d0d3aafc23d8c2195f54be705218cff3647
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-04 16:22:10 +0530

    modify the app file code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import { useDispatch } from 'react-redux';
-import './App.css';
-import { addTodo } from './actions';
-import { useState } from 'react';
+import { useDispatch } from "react-redux";
+import { addTodo } from "./actions";
+import { useState } from "react";
+import TodoList from "./TodoList"
 
 function App() {
-
   const dispatch = useDispatch();
-  const [todoTitle, setTodoTitle] = useState("")
 
-  const handleTodo = ()=>{
-    if(todoTitle.trim()){
+  const [todoTitle, setTodoTitle] = useState("");
+  const handleTodo = () => {
+    if (todoTitle.trim()) {
       const newTodo = {
-        title: todoTitle
-      }
-
-      dispatch(addTodo(newTodo))
-      setTodoTitle("")
+        title: todoTitle,
+      };
+      dispatch(addTodo(newTodo));
+      setTodoTitle("");
     }
-  }
-
+  };
 
   return (
-    <div className='container mt-5 w-50'>
-      <h3>Todo App using redux react</h3>
-      <div className='input-group'>
-        <input type='text'
-                className='form-control'
-                value={todoTitle}
-                onChange={(e)=>{
-                  setTodoTitle(e.target.value)
-                }}/>
-        <button className='btn btn-primary' onClick={()=> handleTodo()}>Add</button>
+    <div className="container mt-5 w-50">
+      <h3 className="text-primary text-center">Todo app using redux react</h3>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={() => handleTodo()}>
+          Add
+        </button>
       </div>
+      <TodoList />
     </div>
   );
 }
