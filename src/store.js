52fea3e65747c4302cd446007026e3539c791df1
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-04 06:46:15 +0530

    modify the reducer function cases to return newstate based on the action functions

--- src/store.js
+++ src/store.js
@@ -8,11 +8,22 @@ const initialState = {
 function todoReducer(state=initialState, action){
     switch(action.type){
         case "ADD_TODO":
-            return{}
+            const newTodo = {id:state.nextId, title: action.payload.title}
+            return{
+                ...state, todos:[...state.todos, newTodo], nextId: state.nextId + 1
+            }
         case "EDIT_TODO":
-            return{}
+            return{
+                ...state, todos: state.todos.map(
+                    (todo)=>todo.id === action.payload.id?{...todo, title:action.payload.title}:todo
+                )
+            }
         case "DELETE_TODO":
-            return{}
+            return{
+                ...state, todos: state.todos.filter(
+                    (todo)=>todo.id !== action.payload.id
+                )
+            }
         default:
             return state
     }
