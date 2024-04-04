a3ecf2292887d293d3481d43d5300fbdf7b56e3f
NaveenReddy0427 <naveendeva909@gmail.com>
2024-04-04 16:25:11 +0530

    modify some code in TodoList file, overall the code is ready to run todo app

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux";
-import Todo from "./todo";
+import Todo from "./Todo";
 
 function TodoList() {
   const todos = useSelector((state) => state.todos);
+
+  console.log(todos);
   return (
-      <ul className="list-group mt-4">
-        {todos.map((todo)=>(
-            <Todo key={todo.id} id={todo.id} title={todo.title}/>
-        ))}
-      </ul>
+    <ul className="list-group mt-4">
+      {todos.map((todo) => (
+        <Todo key={todo.id} id={todo.id} title={todo.title} />
+      ))}
+    </ul>
   );
 }
 
